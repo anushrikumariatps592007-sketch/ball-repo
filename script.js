cb611740fd5eda4d5dc4287322e96c54a6b66359
anushri kumari <anushrikumariatps592007@gmail.com>
2026-07-03 15:05:49 +0530

    add the script.js code

---
+++ script.js
@@ -0,0 +1,15 @@
+
+gsap.to (".ball",{
+    y:-250,
+    delay:1,
+    duration:1,
+    repeat:-1,
+    yoyo:true,
+})
+gsap.to (".shadow",{
+    scale:0.2,
+    delay:1,
+    duration:1,
+    repeat:-1,
+    yoyo:true,
+})
